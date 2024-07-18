192497042333e888effc01e745269996f3803e4f
Reborn <670890543@qq.com>
2024-07-18 17:39:28 +0800

    reset password bancken

--- backend/src/main/java/com/example/backend/controller/AuthorizeController.java
+++ backend/src/main/java/com/example/backend/controller/AuthorizeController.java
@@ -1,7 +1,9 @@
 package com.example.backend.controller;
 
 import com.example.backend.entity.RestBean;
+import com.example.backend.entity.vo.request.ConfirmResetVO;
 import com.example.backend.entity.vo.request.EmailRegisterVO;
+import com.example.backend.entity.vo.request.EmailResetVO;
 import com.example.backend.service.AccountService;
 import jakarta.annotation.Resource;
 import jakarta.servlet.http.HttpServletRequest;
@@ -11,6 +13,8 @@ import jakarta.validation.constraints.Pattern;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Supplier;
 
 @Validated
@@ -21,6 +25,13 @@ public class AuthorizeController {
     @Resource
     AccountService service;
 
+    /**
+     * 获取验证码
+     * @param email
+     * @param type
+     * @param request
+     * @return
+     */
     @GetMapping("/ask-code")
     public RestBean<Void> askVerifyCode(@RequestParam @Email String email,
                                         @RequestParam @Pattern(regexp = "(register|reset)") String type,
@@ -29,9 +40,28 @@ public class AuthorizeController {
             service.registerEmailVerifyCode(type, email, request.getRemoteAddr()));
     }
 
+    /**
+     * 注册
+     * @param vo
+     * @return
+     */
     @PostMapping("/register")
     public RestBean<Void> register(@RequestBody @Valid EmailRegisterVO vo){
         return this.messageHandle(()-> service.registerEmailAccount(vo));
+    }
+
+    @PostMapping("/reset-confirm")
+    public RestBean<Void> resetConfirm(@RequestBody @Valid ConfirmResetVO vo){
+        return this.messageHandle(vo,service::resetConfirm);
+    }
+
+    @PostMapping("/reset-password")
+    public RestBean<Void> resetPassword(@RequestBody @Valid EmailResetVO vo){
+        return this.messageHandle(vo,service::resetEmailAccountPassword);
+    }
+
+    private <T> RestBean<Void> messageHandle(T vo, Function<T,String> function){
+        return messageHandle(()->function.apply(vo));
     }
 
     private RestBean<Void> messageHandle(Supplier<String> action){
--- backend/src/main/java/com/example/backend/service/impl/AccountServiceImpl.java
+++ backend/src/main/java/com/example/backend/service/impl/AccountServiceImpl.java
@@ -3,7 +3,9 @@ package com.example.backend.service.impl;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.example.backend.entity.dto.Account;
+import com.example.backend.entity.vo.request.ConfirmResetVO;
 import com.example.backend.entity.vo.request.EmailRegisterVO;
+import com.example.backend.entity.vo.request.EmailResetVO;
 import com.example.backend.mapper.AccountMapper;
 import com.example.backend.service.AccountService;
 import com.example.backend.utils.Const;
@@ -115,5 +117,37 @@ public class AccountServiceImpl extends ServiceImpl<AccountMapper, Account> impl
     }
     private boolean existAccountByUsername(String username){
         return this.baseMapper.exists(Wrappers.<Account>query().eq("username",username));
+    }
+
+    /**
+     * 重置密码验证
+     * @param vo
+     * @return
+     */
+    @Override
+    public String resetConfirm(ConfirmResetVO vo) {
+        String email = vo.getEmail();
+        String code = stringRedisTemplate.opsForValue().get(Const.VERIFY_EMAIL_DATA + email);
+        if(code == null) return "请先获取验证码";
+        if(!code.equals(vo.getCode())) return "验证码错误，请重试";
+        return null;
+    }
+
+    /**
+     * 重置密码
+     * @param vo
+     * @return
+     */
+    @Override
+    public String resetEmailAccountPassword(EmailResetVO vo) {
+        String email = vo.getEmail();
+        String verify = this.resetConfirm(new ConfirmResetVO(email,vo.getCode()));
+        if(verify != null) return verify;
+        String password = encoder.encode(vo.getPassword());
+        boolean update = this.update().eq("email",email).set("password",password).update();
+        if (update){
+            stringRedisTemplate.delete(Const.VERIFY_EMAIL_DATA + email);
+        }
+        return null;
     }
 }
